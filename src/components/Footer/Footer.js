3044a84076fa5ff622c53983843e9a44187110c8
mbwatson <mattwat@gmail.com>
2018-09-07 11:17:18 -0400

    add rel noopener noreferrer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,13 @@ const footer = ( props ) => {
     return (
         <footer>
             <div className="footer__box">
-                <a href="https://nihdatacommons.us/" target="_blank">
+                <a href="https://nihdatacommons.us/" target="_blank" rel="noopener noreferrer">
                     <img src={ dcppcLogo } alt="DCPPC Logo" />
                 </a>
             </div>
             <div className="footer__box">
                 View our Documentation on
-                <a href="https://github.com/dcppc/dcppc-phase1-demos/blob/master/demo-list.rst" target="_blank"><FontAwesomeIcon icon={ ['fab', 'github'] } /> GitHub</a>
+                <a href="https://github.com/dcppc/dcppc-phase1-demos/blob/master/demo-list.rst" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={ ['fab', 'github'] } /> GitHub</a>
             </div>
         </footer>
     )

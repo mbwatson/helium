7c431dacf65b758192aa099e4a04dbebd39838b7
mbwatson <mattwat@gmail.com>
2018-09-12 11:00:25 -0400

    add color property to ContentBlock component

--- src/components/ContentBlock/ContentBlock.js
+++ src/components/ContentBlock/ContentBlock.js
@@ -4,7 +4,7 @@ import './ContentBlock.scss'
 const contentBlock = ( props ) => {
     return (
         <div className={ "content-block"}
-            style={{ backgroundColor: `var(--color-${props.color})` }} >
+            style={{ backgroundColor: `var(--color-${ props.color })` }} >
             { props.children }
         </div>
     )

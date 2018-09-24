1108a152ff4204611c180dbbb484fd09fba7e15b
mbwatson <mattwat@gmail.com>
2018-09-24 11:28:00 -0400

    add font

--- src/index.js
+++ src/index.js
@@ -5,10 +5,29 @@ import './index.css';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#349ed3',
+        },
+        secondary: {
+            main: '#54cef3',
+        },
+        error: {
+            main: '#ff0000',
+        }
+    },
+});
+
 ReactDOM.render(
-    <Router basename={ '/helium' }>
-        <App />
-    </Router>,
-    document.getElementById('root'));
+    <MuiThemeProvider theme={ theme }>
+        <Router basename={ '/helium' }>
+            <App />
+        </Router>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+);
 
 registerServiceWorker();

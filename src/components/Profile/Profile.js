b811dac90551271efdf60efe89bd3e641acf55f7
mbwatson <mattwat@gmail.com>
2018-09-28 13:33:29 -0400

    extract profile data to array

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,13 +3,14 @@ import { withStyles } from '@material-ui/core/styles'
 import { Card, CardActionArea, CardMedia, CardContent } from '@material-ui/core'
 
 import Paragraph from '../Typography/Paragraph'
+import Heading from '../Typography/Heading'
 
 const styles = ( theme ) => ({
     card: {
-        maxWidth: 345,
+        maxWidth: 300,
     },
     media: {
-        height: 140,
+        height: 150,
     },
 })
 
@@ -23,51 +24,17 @@ const profile = ( props ) => {
                     image={ props.imageSrc }
                     title={ props.name }
                 />
+                <CardContent>
+                    <Heading>
+                        { props.name }
+                    </Heading>
+                    <Paragraph>
+                        { props.bio }
+                    </Paragraph>
+                </CardContent>
             </CardActionArea>
-            <CardContent>
-                <Paragraph>
-                    { props.bio }
-                </Paragraph>
-            </CardContent>
         </Card>
     )
 }
 
 export default withStyles(styles)(profile)
-
-
-    // root: {
-    //     border: '1px solid #eee',
-    //     boxShadow: '0 5px 10px rgba(0, 0, 0, 0.05)',
-    //     padding: '1rem',
-    //     marginBottom: '2rem',
-    //     textAlign: 'center',
-    //     display: 'flex',
-    //     flexDirection: 'row',
-    //     '&:hover $image': {
-    //         opacity: 1,
-    //         transform: 'scale(1.1)',
-    //         marginRight: '1rem',
-    //     },
-    //     imageContainer: {
-    //         display: 'inline-block',
-    //         borderRadius: '50%',
-    //         clipPath: 'circle(50%)',
-    //     },
-    //     image: {
-    //         display: 'block',
-    //         opacity: '0.75',
-    //         transition: 'opacity 250ms, transform 500ms ease-out',
-    //     },
-    //     details: {
-    //         flex: 1,
-    //     },
-    //     name: {
-    //         textAlign: 'left',
-    //         padding: '0 1rem',
-    //     },
-    //     bio: {
-    //         textAlign: 'left',
-    //         padding: '0 1rem',
-    //     },
-    // }

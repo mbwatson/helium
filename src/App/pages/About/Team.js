b60625cf4b509f756dbc4fd5ac097d5f2f25ae41
mbwatson <mattwat@gmail.com>
2018-09-28 13:36:24 -0400

    add title and organization to profile data

--- src/App/pages/About/Team.js
+++ src/App/pages/About/Team.js
@@ -12,28 +12,53 @@ const styles = ( theme ) => ({
 
 const teamPage = ( props ) => {
     const { classes } = props
+    const team = [
+        {
+            name: 'Alice Anderson',
+            imageSrc: 'https://picsum.photos/200/200?random',
+            title: 'Job Title',
+            organization: 'Organization, Inc.',
+            bio: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore.
+                Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?`,
+        },
+        {
+            name: 'Bob Bobbins',
+            imageSrc: 'https://picsum.photos/201/201?random',
+            title: 'Job Title',
+            organization: 'Organization, Inc.',
+            bio: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore.
+                Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?`,
+        },
+        {
+            name: 'Carl Conner',
+            imageSrc: 'https://picsum.photos/202/202?random',
+            title: 'Job Title',
+            organization: 'Organization, Inc.',
+            bio: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore.
+                Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?`,
+        },
+    ]
+
     return (
         <Container className={ classes.root }>
 
             <Title>Meet Team Helium</Title>
-
-            <Profile
-                name='Alice Anderson'
-                imageSrc='https://picsum.photos/200/200?random'
-                bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore. Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?'
-            />
-
-            <Profile
-                name='Bob Bobbins'
-                imageSrc='https://picsum.photos/201/201?random'
-                bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore. Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?'
-            />
             
-            <Profile
-                name='Carl Conner'
-                imageSrc='https://picsum.photos/202/202?random'
-                bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci omnis voluptatem possimus expedita ut, vero perspiciatis labore. Cumque quasi ut modi accusamus, ullam voluptatem, cum recusandae consequatur vero rem adipisci?'
-            />
+            {
+                team.map( (person, index) => {
+                    return (
+                        <Profile
+                            key={ index }
+                            name={ person.name }
+                            imageSrc={ person.imageSrc }
+                            bio={ person.bio }
+                        />
+                    )
+                })
+            }
 
         </Container>
     )

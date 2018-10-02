523cfd6a5c3d52d0e8bd3e7be2f6b9a6a2f7877f
mbwatson <mattwat@gmail.com>
2018-10-02 15:02:03 -0400

    move animation into class definitions

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = ( theme ) => ({
+    shell: {
+        border: '1px solid #f00',
+        transformOrigin: '20px 50px',
+        animationTimingFunction: 'linear',
+        'animation': 'spin 10s infinite',
+    },
     nucleus: {
         fill: theme.palette.secondary.main,
     },
-    orbital: {
+    path: {
         fill: 'transparent',
         stroke: theme.palette.primary.main,
     },
@@ -20,19 +26,20 @@ const styles = ( theme ) => ({
         fill: theme.palette.primary.main,
         stroke: 'transparent',
     },
+    '@keyframes spin': {
+        'from': { transform: 'rotate(0deg)', },
+        'to': { transform: 'rotate(179deg)', },
+    },
 })
 
 const brand = ( props ) => {
     const { classes } = props
     return (
         <svg width="125px" height="75px" viewBox="0 0 125 100">
-            <g id="helium-atom">
-                <circle className={ classes.orbital } cx="20" cy="50" r="35" strokeWidth="1"></circle>
+            <g className={ classes.shell } >
+                <circle className={ classes.path } cx="20" cy="50" r="35" strokeWidth="1"></circle>
                 <circle className={ classes.electron } cx="-15" cy="50" r="6"></circle>
                 <circle className={ classes.electron } cx="55" cy="50" r="6"></circle>
-                <animateTransform attributeName="transform" attributeType="XML"
-                    type="rotate" from="0 20 50" to="180 20 50"
-                    begin="0s" dur="10s" repeatCount="indefinite"/>
             </g>
             <text x="20" y="97" className={ classes.text }>He</text>
             <circle className={ classes.nucleus } cx="20" cy="50" r="10"></circle>

0888b2395a91d3a2a3a82bd0d045ce59ddabc1ad
mbwatson <mattwat@gmail.com>
2018-10-03 09:03:21 -0400

    add Float component

--- src/App/pages/About/Overview.js
+++ src/App/pages/About/Overview.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react'
-import { Grid } from '@material-ui/core'
 
 import Title from '../../../components/Typography/Title'
 import Paragraph from '../../../components/Typography/Paragraph'
 import TextLink from '../../../components/Links/TextLink'
+
+import Float from '../../../components/Layout/Float'
 
 import GraphicPlaceholder from '../../../devComponents/GraphicPlaceholder'
 
@@ -14,6 +15,8 @@ import UnmLogo from '../../../images/logos/univ-new-mexico.jpeg'
 import OsuLogo from '../../../images/logos/oregon-state-univ.png'
 import JaxLabLogo from '../../../images/logos/jackson-lab.png'
 import MaastrichtLogo from '../../../images/logos/maastricht-univ.png'
+
+import DcppcLogo from '../../../images/logos/dcppc.svg'
 
 const overviewPage = ( props ) => {
     const logoGrid = {
@@ -75,24 +78,25 @@ const overviewPage = ( props ) => {
             
             <GraphicPlaceholder height="200px" text="Cloud-based Architecture"/>
             
-            <Grid container spacing={ 32 }>
-                <Grid item xs={ 12 } sm={ 6 } md={ 8 } style={ { display: 'flex', alignItems: 'center', justifyContent: 'center', } }>
-                    <Paragraph>
-                        The initial implementation is a Pilot Phase in which
-                        targeted <TextLink new to="https://commonfund.nih.gov/commons/testcasedatasets">high-value data sources</TextLink> will
-                        serve as test cases for the infrastructure to be developed.
-                        The test datasets come from the <TextLink new to="https://commonfund.nih.gov/gtex">Genotype-Tissue Expression</TextLink> (GTEx) and
-                        the <TextLink new to="https://www.nhlbi.nih.gov/science/trans-omics-precision-medicine-topmed-program">Trans-Omics for Precision Medicine</TextLink> (TOPMed) efforts,
-                        as well as several Model Organism Databases (MODs).
-                        Particular attention will be paid to ensuring that the resources produced by the NIH Data Commons Pilot will adhere to FAIR principles.
-                        This work is being carried out by the <TextLink new to="https://nihdatacommons.us/">Data Commons Pilot Phase Consortium</TextLink> (DCPPC).
-                    </Paragraph>
-                </Grid>
-                <Grid item xs={ 12 } sm={ 6 } md={ 4 } style={ { display: 'flex', alignItems: 'center', justifyContent: 'center', } }>
-                    <GraphicPlaceholder height="300px" text="DCPPC"/>
-                </Grid>
-            </Grid>
+            <Paragraph>
+                The initial implementation is a Pilot Phase in which
+                targeted <TextLink new to="https://commonfund.nih.gov/commons/testcasedatasets">high-value data sources</TextLink> will
+                serve as test cases for the infrastructure to be developed.
+                
+                
+                The test datasets come from the <TextLink new to="https://commonfund.nih.gov/gtex">Genotype-Tissue Expression</TextLink> (GTEx) and
+                the <TextLink new to="https://www.nhlbi.nih.gov/science/trans-omics-precision-medicine-topmed-program">Trans-Omics for Precision Medicine</TextLink> (TOPMed) efforts,
+                as well as several Model Organism Databases (MODs).
 
+                
+                Particular attention will be paid to ensuring that the resources produced by the NIH Data Commons Pilot will adhere to FAIR principles.
+                This work is being carried out by the <TextLink new to="https://nihdatacommons.us/">Data Commons Pilot Phase Consortium</TextLink> (DCPPC).
+            </Paragraph>
+            
+            <Float left>
+                <img src={ DcppcLogo }/>
+            </Float>
+            
             <Paragraph>
                 This vision requires the efforts of a multidisciplinary investigative team with complementary expertise in biomedicine, cloud architecture, software engineering, analytical tools, and data harmonization. Each of eight defined Key Capabilities (KCs) addresses specific challenges faced by scientists working with large-scale biomedical data. The proposed projects are designed so that each KC has unique objectives and deliverables in the form of stand-alone demonstrations and products.
                 Yet together, the KCs form a continuum of insights and approaches that capture the five "V"s of data (Velocity, Volume, Value, Variety, and Veracity) and reflect FAIR principles (Findable, Accessible, Interoperable, and Reusable).
---
+++ src/components/Layout/Float.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const float = ( props ) => {
+    const style = {
+        float: 'none',
+        margin: '1rem',
+    }
+    // Left or right
+   if (props.left) {
+        style.float = 'left'
+        style.marginLeft = 0
+    }
+    if (props.right) {
+        style.float = 'right'
+        style.marginRight = 0
+    }
+    // Responsiveness
+    return (
+        <div style={ style }>
+            { props.children }
+        </div>
+    )
+}
+
+export default float

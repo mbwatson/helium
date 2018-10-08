7fd5da22a1597dfee9dffaa9de6eb42792e621d3
mbwatson <mattwat@gmail.com>
2018-10-08 10:55:37 -0400

    remove font-awesome icons

--- src/App/App.js
+++ src/App/App.js
@@ -8,12 +8,6 @@ import PlusPage from './pages/Plus/Plus'
 
 import './App.scss'
 
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faRocket, faFlask, faUsers } from '@fortawesome/free-solid-svg-icons'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
-
-// library.add(faRocket, faFlask, faUsers, fab)
-
 class App extends Component {
 
     render() {
